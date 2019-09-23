e29108e914a350bcb99b43de870ce812143a947e
nutcakez <knorbert91@gmail.com>
2019-09-23 21:54:13 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -13,21 +13,20 @@ var rooms=[];
 var users=[];
 
 var lobbyusers=[];
-
+var game=[];
 
 var io=require('socket.io')(server,{});
 io.sockets.on('connection',function(socket){
     console.log("socket connection "+socket.id);
-    users.push(socket.id);
+    users.push({
+        'username':socket.id,
+        'currentroom':''
+    });
     SendAvailableRooms(socket);
-    socket.emit('test');
-    socket.on('join',function(data){
-        console.log(data.roomnumber)
-    })
 
     socket.on('showrooms',function(){
         console.log("server got the showrooms message")
-        socket.emit('rooms',{'rooms':rooms});
+        socket.emit('rooms',rooms);
         })
 
 
@@ -35,9 +34,31 @@ io.sockets.on('connection',function(socket){
         CreateNewRoom(socket.id);
         console.log(rooms.length);
         SendAvailableRooms(io);
-
+        io.emit('rooms', rooms);
     })
     
+    socket.on('joinroom',function(data){
+        console.log("receiving joinroom attempt")
+        console.log(rooms.length);
+        console.log("find room outcome: "+FindRoom(data.room,rooms));
+        if(FindRoom(data.room,rooms)>-1){
+            socket.join(data.room);
+            console.log("this is the index of it "+IndexOfRoom(data.room,rooms));
+            rooms[IndexOfRoom(data.room,rooms)].users.push(socket.id);
+        }
+        else
+        {
+            console.log("no such room")
+        }
+        console.log(socket.id+"  user joined this room: "+data.room);
+        console.log("current players inside: "+rooms[IndexOfRoom(data.room,rooms)].users)
+    })
+
+    socket.on('delayed',function(){
+        var t=setTimeout(function(){
+            io.emit('delayedmessage','potato');
+        }, 3500);
+    })
 
    
 })
@@ -46,9 +67,14 @@ io.sockets.on('connection',function(socket){
 
 function CreateNewRoom(username){
     rooms.push({
+        "roomid":username,
         "users":[username],
         "visible":[true]
     })
+}
+
+function CreateGame(username){
+
 }
 
 function SendAvailableRooms(socket){
@@ -59,23 +85,23 @@ function SendAvailableRooms(socket){
 }
 
 
-
-
-
-
-class Room{
-    constructor(roomID,userID){
-        this.roomID=roomID;
-        this.userID=userID;
-        this.members=[];
-        this.members.push(userID);
-        this.visible=true;
+function IndexOfRoom(roomid,roomarray){
+    for (var i = roomarray.length - 1; i >= 0; i--) {
+        if(roomarray[i].roomid==roomid){
+            return i;
+        }
     }
-    UserJoin(userID) {
-        this.members.push(userID);
-        this.visible=false;    
-    }
-    UserLeave(userID){
-        
-    }
+    return -1;
+}
+
+function FindRoom(roomid,roomarray){
+    var idtoreturn=-1;
+    roomarray.forEach(function(item,index){
+        console.log(index+"   "+item.roomid);
+        if(item.roomid==roomid && idtoreturn==-1){
+            console.log("found");
+            idtoreturn=index;
+        }
+    })
+    return idtoreturn;
 }
